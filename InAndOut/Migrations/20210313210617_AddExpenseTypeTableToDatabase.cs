fbce1c4a423aaca058ff47c70b0f5fafa5e4b9b4
adriantwardy <adriantwardawski@gmail.com>
2021-03-14 00:42:15 +0100

    Add new files

---
+++ InAndOut/Migrations/20210313210617_AddExpenseTypeTableToDatabase.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InAndOut.Migrations
+{
+    public partial class AddExpenseTypeTableToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExpensesCategories");
+
+            migrationBuilder.CreateTable(
+                name: "ExpensesTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExpensesTypes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExpensesTypes");
+
+            migrationBuilder.CreateTable(
+                name: "ExpensesCategories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MostExpense = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NiceToHave = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Useless = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExpensesCategories", x => x.Id);
+                });
+        }
+    }
+}
---
+++ InAndOut/Models/ExpenseType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Models
+{
+    public class ExpenseType
+    {
+        [Key]
+        public int Id { get; set; }
+        [DisplayName("Expense Type")]
+        [Required]
+        [MinLength(2)]
+        public string Name { get; set; }
+        
+        
+    }
+}
